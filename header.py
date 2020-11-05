97c6dd9e44bf1279433b1241998c691c435589a5
Diego Santos Seabra <dss-diego@hotmail.com>
2020-11-05 13:29:08 -0300

    titulo pagina web

--- header.py
+++ header.py
@@ -3,7 +3,7 @@ import streamlit as st
 def header():
     # Define as configurações da página
     st.beta_set_page_config(
-        page_title="Trabalho 1 - Sistemas Operacionais",
+        page_title="Trabalho 2 - Sistemas Operacionais",
     )
 
     st.title("Trabalho 2 - Substituição de Páginas")
